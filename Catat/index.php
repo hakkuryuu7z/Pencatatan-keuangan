0a9a6e4957f4a960c9a8f399c47d4b4a9a055b36
hakkuryuu7z <muhammadwildansafrudin@gmail.com>
2026-03-19 22:39:33 +0700

    perbaikan navbar

---
+++ Catat/index.php
@@ -0,0 +1,15 @@
+<?php
+include "../config/database.php";
+session_start();
+if (!isset($_SESSION['login'])) {
+    header("Location:../auth/login.php");
+    exit();
+}
+
+?>
+
+<?php include "../templates/header.php" ?>
+
+<?php include "../templates/navbar.php" ?>
+
+<?php include "../templates/footer.php" ?>
--- templates/navbar.php
+++ templates/navbar.php
@@ -10,10 +10,10 @@
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                 <li class="nav-item">
-                    <a class="nav-link active" href="index.php">Home</a>
+                    <a class="nav-link active" href="../dashboard/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="../Catat/">Catat</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Pricing</a>
